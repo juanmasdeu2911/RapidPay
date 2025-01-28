526a93d8e20e09deb4ff4c00100964020c48e9ab
Juan Pablo Masdeu <jpmasdeu@gmail.com>
2025-01-28 13:15:54 -0300

    Fixed documentation for swagger

--- RapidPay.Tests/CardRepositoryTests.cs
+++ RapidPay.Tests/CardRepositoryTests.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using RapidPay.DAL.Data;
 using RapidPay.DAL.Models;
 using RapidPay.DAL.Repositories;
@@ -8,131 +7,92 @@ namespace RapidPay.Tests.Repositories
 {
     public class CardRepositoryTests
     {
-        private readonly Mock<ApplicationDbContext> _mockContext;
-        private readonly Mock<DbSet<Card>> _mockDbSet;
+        private readonly ApplicationDbContext _context;
         private readonly CardRepository _repository;
 
         public CardRepositoryTests()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                              .UseInMemoryDatabase(databaseName: "TestDatabase")
+                              .UseSqlServer("Server=.\\rapidpayexpress;Database=RapidPayTest;User ID=sa;Password=admin1234;Encrypt=True;TrustServerCertificate=True")
                               .Options;
-            _mockContext = new Mock<ApplicationDbContext>(options);
-            _mockDbSet = new Mock<DbSet<Card>>();
-            _mockContext.Setup(m => m.Set<Card>()).Returns(_mockDbSet.Object);
 
-            _repository = new CardRepository(_mockContext.Object);
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            SeedDatabase();
+
+            _repository = new CardRepository(_context);
+
+        }
+
+        private void SeedDatabase()
+        {
+            _context.Cards.Add(new Card("3456789012345", 25000));
+            _context.Cards.Add(new Card("32109876543210", 50000));
+            _context.SaveChanges();
+
         }
 
         [Fact]
         public async Task CreateCardAsync_ShouldAddCard()
         {
             // Arrange
-            var card = new Card("123456789012345", 1000);
+            var card = new Card("123456789011345", 1000);
 
             // Act
-            await _repository.CreateCardAsync(card);
+            var result = await _repository.CreateCardAsync(card);
 
             // Assert
-            _mockDbSet.Verify(m => m.AddAsync(card, default), Times.Once);
-            _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Once);
+            Assert.Equal(card, result);
+            Assert.Contains(card, _context.Cards);
         }
 
         [Fact]
         public async Task GetCardAsync_ShouldReturnCard_WhenCardExists()
         {
-            // Arrange
-            var cardId = 1;
-            var card = new Card("123456789012345", 1000);
-            typeof(Card).GetProperty("Id")?.SetValue(card, cardId);
-            var data = new List<Card> { card }.AsQueryable();
+           
+            var result = await _repository.GetCardAsync("3456789012345");
 
-            var mockAsyncEnumerable = new Mock<IAsyncEnumerable<Card>>();
-            var mockAsyncEnumerator = new Mock<IAsyncEnumerator<Card>>();
-
-            mockAsyncEnumerator.Setup(m => m.Current).Returns(data.First());
-            mockAsyncEnumerator.SetupSequence(m => m.MoveNextAsync())
-                               .ReturnsAsync(true)
-                               .ReturnsAsync(false);
-            mockAsyncEnumerator.Setup(m => m.DisposeAsync()).Returns(ValueTask.CompletedTask);
-
-            mockAsyncEnumerable.Setup(m => m.GetAsyncEnumerator(default)).Returns(mockAsyncEnumerator.Object);
-
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Provider).Returns(data.Provider);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Expression).Returns(data.Expression);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-            _mockDbSet.As<IAsyncEnumerable<Card>>().Setup(m => m.GetAsyncEnumerator(default)).Returns(mockAsyncEnumerator.Object);
-
-            // Act
-            var result = await _repository.GetCardByIdAsync(cardId);
-
-            // Assert
-            Assert.Equal(card, result);
-
-
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal("3456789012345", result.Number);
         }
 
         [Fact]
         public async Task GetCardByIdAsync_ShouldReturnCard_WhenCardExists()
         {
-            // Arrange
-            var cardId = 1;
-            var card = new Card("123456789012345", 1000);
-            typeof(Card).GetProperty("Id").SetValue(card, cardId);
-            var data = new List<Card> { card }.AsQueryable();
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Provider).Returns(data.Provider);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Expression).Returns(data.Expression);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-            // Act
-            var result = await _repository.GetCardByIdAsync(cardId);
+            var result = await _repository.GetCardByIdAsync(1);
 
-            // Assert
-            Assert.Equal(card, result);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Id);
+            Assert.Equal("3456789012345", result.Number);
         }
 
         [Fact]
         public async Task UpdateCardAsync_ShouldUpdateCard_WhenCardExists()
         {
-            // Arrange
-            var cardId = 1;
-            var card = new Card("123456789012345", 1000);
-            typeof(Card).GetProperty("Id").SetValue(card, cardId);
-            var data = new List<Card> { card }.AsQueryable();
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Provider).Returns(data.Provider);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.Expression).Returns(data.Expression);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            _mockDbSet.As<IQueryable<Card>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            //Arrange
+            var existingCard = await _repository.GetCardByIdAsync(1);
+            existingCard.Balance = 15000;
 
-            var updatedCard = new Card("543210987654321", 500);
-            typeof(Card).GetProperty("Id").SetValue(updatedCard, cardId);
-
-            // Act
-            var result = await _repository.UpdateCardAsync(updatedCard);
-
-            // Assert
+            //Act
+            var result = await _repository.UpdateCardAsync(existingCard);
+            //Assert
             Assert.True(result);
-            Assert.Equal(updatedCard.Number, card.Number);
-            Assert.Equal(updatedCard.Balance, card.Balance);
-            _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Once);
         }
 
         [Fact]
         public async Task UpdateCardAsync_ShouldReturnFalse_WhenCardDoesNotExist()
         {
-            // Arrange
-            var cardId = 1;
-            var updatedCard = new Card("543210987654321", 500);
-            typeof(Card).GetProperty("Id").SetValue(updatedCard, cardId);
 
             // Act
-            var result = await _repository.UpdateCardAsync(updatedCard);
-
-            // Assert
-            Assert.False(result);
-            _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Never);
+            var existingCard = await _repository.GetCardByIdAsync(3);
+                        
+            //Assert
+            Assert.Null(existingCard);
         }
     }
 }
